39ad11ecd4cd5d93b81ab75b729ba7740b5795ed
Tuan Pham <tuan.pham@cerebral.com>
2023-06-08 23:12:03 +0700

    Try deploy

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t capstone-app .
+docker build -t ml-classification .
 
 # Step 2:
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3:
 # Run app
-docker run -p 80:80 capstone-app
+docker run -p 80:80 ml-classification
