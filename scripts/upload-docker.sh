f3dabc89ab7edd00a727f63c9a4341bf622c65f0
Tuan Pham <tuan.pham@cerebral.com>
2023-06-08 12:42:50 +0700

    Add scripts for manual reference & deployment

---
+++ scripts/upload-docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="phamanhtuan221995/ml-classification"
+
+
+# Step 2:
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+# docker login --username sabbir33
+docker image tag ml-classification $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
